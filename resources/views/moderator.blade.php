315027f669b70d65348d963656b5c310f962d5fe
arsen <turelyk.as@gmail.com>
2023-05-17 16:46:29 +0200

    17/05/2023 list user coment button

--- resources/views/moderator.blade.php
+++ resources/views/moderator.blade.php
@@ -44,7 +44,7 @@
             @endforeach
         </div>
     </section> --}}
-
+    
     <div class="bg-slate-50 py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl lg:mx-0">
@@ -95,10 +95,55 @@
                                 <p class="text-gray-600">{{ $post->user->role }}</p>
                             </div>
                         </div>
+                        
+                        {{-- <section class="space-y-6">
+                            <x-danger-button x-data=""
+                                x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion-{{ $post->id }}')">
+                                {{ __('Delete post') }}</x-danger-button>
+    
+                            <x-modal :name="'confirm-user-deletion-' . $post->id" :show="$errors->userDeletion->isNotEmpty()" focusable>
+                                <form method="post" action="{{ route('profile.deleteUser', $post->id) }}"
+                                    class="p-6">
+                                    <form method="post" action="{{ route('posts.destroy', $post->id) }}" class="p-6">
+
+                                    @csrf
+                                    @method('delete')
+    
+                                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                        {{ __('Are you sure you want to delete your account?') }}
+                                    </h2>
+    
+                                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                        {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                                    </p>
+    
+                                    <div class="mt-6">
+                                        <x-input-label for="password" value="{{ __('Password') }}"
+                                            class="sr-only" />
+    
+                                        <x-text-input id="password" name="password" type="password"
+                                            class="mt-1 block w-3/4" placeholder="{{ __('Password') }}" />
+    
+                                        <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                                    </div>
+    
+                                    <div class="mt-6 flex justify-end">
+                                        <x-secondary-button x-on:click="$dispatch('close')">
+                                            {{ __('Cancel') }}
+                                        </x-secondary-button>
+    
+                                        <x-danger-button class="ml-3">
+                                            {{ __('Delete Account') }}
+                                        </x-danger-button>
+                                    </div>
+                                </form>
+                            </x-modal>
+                        </section> --}}
                     </article>
                 </div>
             @endforeach
         </div>
+        
     </div>
     {{-- list of friend --- user --}}
     {{-- <section class="relative py-20 overflow-hidden bg-white">
