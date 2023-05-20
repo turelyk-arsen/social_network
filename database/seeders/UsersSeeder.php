5ac5e622c51d9e92c06bd076675b800cb1708a31
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-05-20 10:45:26 +0200

    20/05/2023 update photo in users table in db

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -18,42 +18,49 @@ class UsersSeeder extends Seeder
             [
                 'name' => 'admin',
                 'email' => 'admin@example.com',
+                'photo' => 'photos/5MNAF4d5Qnfa1PPF15f1kYwAbxQNvm1jCNEGit7x.jpg',
                 'role' => 'admin',
                 'password' => Hash::make('11111111'),
             ],
             [
                 'name' => 'moderator',
                 'email' => 'moderator@example.com',
+                'photo' => 'photos/qdCoYaxzZjXqqvWtQECInA6DARx9e7nRlweK3CFy.jpg',
                 'role' => 'moderator',
                 'password' => Hash::make('12341234'),
             ],
             [
                 'name' => 'John Doe',
                 'email' => 'johndoe@example.com',
+                'photo' => 'photos/eZMrXExo1NZUCFPAExuwQjY1QMPNnfVmYXOs7Pkp.jpg',
                 'role' => 'user',
                 'password' => Hash::make('asdfasdf'),
             ],
             [
                 'name' => 'Jane Smith',
                 'email' => 'janesmith@example.com',
+                'photo' => 'photos/x7fyd7eufLRpuRUBRy33g2ywME2J8xIr8pAfLyzb.jpg',
                 'role' => 'user',
                 'password' => Hash::make('asdfasdf'),
             ],
             [
                 'name' => 'Arsen',
                 'email' => 'arsen@example.com',
+                'photo' => 'photos/apAoko8mmtWdJdxKDATSAGMktSWHQolAvyAzYEx0.jpg',
                 'role' => 'user',
                 'password' => Hash::make('asdfasdf'),
             ],
             [
                 'name' => 'Vika',
                 'email' => 'vika@example.com',
+                'photo' => '',
                 'role' => 'user',
                 'password' => Hash::make('asdfasdf'),
             ],
             [
                 'name' => 'Ana',
                 'email' => 'ana@example.com',
+                'photo' => '',
                 'role' => 'user',
                 'password' => Hash::make('asdfasdf'),
             ],
@@ -61,4 +68,3 @@ class UsersSeeder extends Seeder
         ]);
     }
     }
-
